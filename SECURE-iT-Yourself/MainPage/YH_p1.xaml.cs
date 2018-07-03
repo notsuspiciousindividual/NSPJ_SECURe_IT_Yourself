f08977563fb5aaa3bc24021fb7e1c3bd2c4d2da9
Blitznoobz <172128W@mymail.nyp.edu.sg>
2018-07-03 12:21:16 +0800

    i created this YH_p1
    I'm starting a new page for my function.

---
+++ SECURE-iT-Yourself/MainPage/YH_p1.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MainPage
+{
+    /// <summary>
+    /// Interaction logic for YH_p1.xaml
+    /// </summary>
+    public partial class YH_p1 : Window
+    {
+        public YH_p1()
+        {
+            InitializeComponent();
+        }
+    }
+}
