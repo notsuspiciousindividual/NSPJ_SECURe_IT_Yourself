4132b64a1b6737e3b1205487c921a2e410f21062
Ninjarku <ninjarku123@gmail.com>
2018-07-04 13:11:48 +0800

    Update on Beck_Option

---
+++ SECURE-iT-Yourself/MainPage/Beck_Option.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MainPage
+{
+    /// <summary>
+    /// Interaction logic for Beck_Option.xaml
+    /// </summary>
+    public partial class Beck_Option : Window
+    {
+        public Beck_Option()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
