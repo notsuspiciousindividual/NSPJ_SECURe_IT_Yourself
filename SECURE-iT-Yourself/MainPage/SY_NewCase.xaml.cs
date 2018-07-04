9f0a1d49dcc9570dd24856d753cbdf9de0985673
Ninjarku <ninjarku123@gmail.com>
2018-07-04 13:34:52 +0800

    Add In some extra stuff

--- SECURE-iT-Yourself/MainPage/SY_NewCase.xaml.cs
+++ SECURE-iT-Yourself/MainPage/SY_NewCase.xaml.cs
@@ -17,9 +17,9 @@ namespace MainPage
     /// <summary>
     /// Interaction logic for Window1.xaml
     /// </summary>
-    public partial class Window1 : Window
+    public partial class SY_NewCase : Window
     {
-        public Window1()
+        public SY_NewCase()
         {
             InitializeComponent();
         }
