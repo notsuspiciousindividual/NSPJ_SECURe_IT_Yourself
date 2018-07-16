b401c7779537b83b89c70c71738abbda6aa29e41
Ninjarku <ninjarku123@gmail.com>
2018-07-16 17:01:04 +0800

    Update Beck_NC

--- SECURE-iT-Yourself/MainPage/Beck_NC.xaml.cs
+++ SECURE-iT-Yourself/MainPage/Beck_NC.xaml.cs
@@ -15,6 +15,8 @@ using System.Windows.Navigation;
 using System.Net;
 using System.Runtime.InteropServices;
 using System.Net.NetworkInformation;
+//using WUApiLib;
+
 
 namespace MainPage
 {
@@ -27,79 +29,308 @@ namespace MainPage
         {
 
             InitializeComponent();
+            //setting overall status Image
+             int stat = -1;
+            ImageSet(stat, CurrentOverallStatImg);
 
         }
-        //Back
+        //Back clicked
         private void BackButton_Click(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("Hi");
-            Initials();
         }
+        //Image Setting
+        private void ImageSet(int Status, Image img)
+        {
+            if (Status.Equals(1))
+            {
+                try
+                {
+                    img.Source = new BitmapImage(new Uri("pack://application:,,,/Resources/Beck_Images/green-smiley-face-md.png"));
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("Problem " + e);
+                }
+            }
+            else if (Status.Equals(0))
+            {
+                try
+                {
+                    //Uri uri = new Uri("pack://application:,,,/Resources/Beck_Images/Background.jpg");
+                    //BitmapImage bi = new BitmapImage(uri);
+                    img.Source = new BitmapImage(new Uri("pack://application:,,,/Resources/Beck_Images/yellow-neutral-face-md.png"));
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("Problem " + e);
+                }
+            }
+            else if (Status.Equals(-1))
+            {
+                try
+                {
+                    //Uri uri = new Uri("pack://application:,,,/Resources/Beck_Images/Background.jpg");
+                    //BitmapImage bi = new BitmapImage(uri);
+                    img.Source = new BitmapImage(new Uri("pack://application:,,,/Resources/Beck_Images/red-smiley-face-md.png"));
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("Problem " + e);
+                }
+            }
+        }
+        //Progress bar
         
-        //respective buttons to configure
+        //For Description
+        private void DescRowBtn1_Click(object sender, RoutedEventArgs e)
+        {
+            if (DescRow1.IsVisible == true)
+            {
+                DescRow1.Visibility = System.Windows.Visibility.Collapsed;
+            }
+            else
+            {
+                DescRow1.Visibility = System.Windows.Visibility.Visible;
+            }
+        }
+        private void DescRowBtn2_Click(object sender, RoutedEventArgs e)
+        {
+            if (DescRow2.IsVisible == true)
+            {
+                DescRow2.Visibility = System.Windows.Visibility.Collapsed;
+            }
+            else
+            {
+                DescRow2.Visibility = System.Windows.Visibility.Visible;
+            }
+        }
+        private void DescRowBtn3_Click(object sender, RoutedEventArgs e)
+        {
+            if (DescRow3.IsVisible == true)
+            {
+                DescRow3.Visibility = System.Windows.Visibility.Collapsed;
+            }
+            else
+            {
+                DescRow3.Visibility = System.Windows.Visibility.Visible;
+            }
+        }
+        private void DescRowBtn4_Click(object sender, RoutedEventArgs e)
+        {
+            if (DescRow4.IsVisible == true)
+            {
+                DescRow4.Visibility = System.Windows.Visibility.Collapsed;
+            }
+            else
+            {
+                DescRow4.Visibility = System.Windows.Visibility.Visible;
+            }
+        }
+        private void DescRowBtn5_Click(object sender, RoutedEventArgs e)
+        {
+            if (DescRow5.IsVisible == true)
+            {
+                DescRow5.Visibility = System.Windows.Visibility.Collapsed;
+            }
+            else
+            {
+                DescRow5.Visibility = System.Windows.Visibility.Visible;
+            }
+        }
+        private void DescRowBtn6_Click(object sender, RoutedEventArgs e)
+        {
+            if (DescRow6.IsVisible == true)
+            {
+                DescRow6.Visibility = System.Windows.Visibility.Collapsed;
+            }
+            else
+            {
+                DescRow6.Visibility = System.Windows.Visibility.Visible;
+            }
+        }
+        private void DescRowBtn7_Click(object sender, RoutedEventArgs e)
+        {
+            if (DescRow7.IsVisible == true)
+            {
+                DescRow7.Visibility = System.Windows.Visibility.Collapsed;
+            }
+            else
+            {
+                DescRow7.Visibility = System.Windows.Visibility.Visible;
+            }
+        }
+        private void DescRowBtn8_Click(object sender, RoutedEventArgs e)
+        {
+            if (DescRow8.IsVisible == true)
+            {
+                DescRow8.Visibility = System.Windows.Visibility.Collapsed;
+            }
+            else
+            {
+                DescRow8.Visibility = System.Windows.Visibility.Visible;
+            }
+        }
+        private void DescRowBtn9_Click(object sender, RoutedEventArgs e)
+        {
+            if (DescRow9.IsVisible == true)
+            {
+                DescRow9.Visibility = System.Windows.Visibility.Collapsed;
+            }
+            else
+            {
+                DescRow9.Visibility = System.Windows.Visibility.Visible;
+            }
+        }
+        private void DescRowBtn10_Click(object sender, RoutedEventArgs e)
+        {
+            if (DescRow10.IsVisible == true)
+            {
+                DescRow10.Visibility = System.Windows.Visibility.Collapsed;
+            }
+            else
+            {
+                DescRow10.Visibility = System.Windows.Visibility.Visible;
+            }
+        }
+        private void DescRowBtn11_Click(object sender, RoutedEventArgs e)
+        {
+            if (DescRow11.IsVisible == true)
+            {
+                DescRow11.Visibility = System.Windows.Visibility.Collapsed;
+            }
+            else
+            {
+                DescRow11.Visibility = System.Windows.Visibility.Visible;
+            }
+        }
+        private void DescRowBtn12_Click(object sender, RoutedEventArgs e)
+        {
+            if (DescRow12.IsVisible == true)
+            {
+                DescRow12.Visibility = System.Windows.Visibility.Collapsed;
+            }
+            else
+            {
+                DescRow12.Visibility = System.Windows.Visibility.Visible;
+            }
+        }
+        //For Respective buttons to configure
         private void StatsRow1_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Hi");
+            MessageBox.Show("Showing "+ConfigRow1.Text.ToString());
+            Beck_Option wnd = new Beck_Option();
+            wnd.Show();
+
+            this.Close();
         }
 
         private void StatsRow2_Click(object sender, RoutedEventArgs e)
         {
+            MessageBox.Show("Showing " + ConfigRow2.Text.ToString());
+            Beck_Option wnd = new Beck_Option();
+            wnd.Show();
 
+            this.Close();
         }
 
         private void StatsRow3_Click(object sender, RoutedEventArgs e)
         {
+            MessageBox.Show("Showing " + ConfigRow3.Text.ToString());
+            Beck_Option wnd = new Beck_Option();
+            wnd.Show();
 
+            this.Close();
         }
 
         private void StatsRow4_Click(object sender, RoutedEventArgs e)
         {
+            MessageBox.Show("Showing " + ConfigRow4.Text.ToString());
+            Beck_Option wnd = new Beck_Option();
+            wnd.Show();
 
+            this.Close();
         }
 
         private void StatsRow5_Click(object sender, RoutedEventArgs e)
         {
+            MessageBox.Show("Showing " + ConfigRow5.Text.ToString());
+            Beck_Option wnd = new Beck_Option();
+            wnd.Show();
 
+            this.Close();
         }
 
         private void StatsRow6_Click(object sender, RoutedEventArgs e)
         {
+            MessageBox.Show("Showing " + ConfigRow6.Text.ToString());
+            Beck_Option wnd = new Beck_Option();
+            wnd.Show();
 
+            this.Close();
         }
         private void StatsRow7_Click(object sender, RoutedEventArgs e)
         {
+            MessageBox.Show("Showing " + ConfigRow7.Text.ToString());
+            Beck_Option wnd = new Beck_Option();
+            wnd.Show();
 
+            this.Close();
         }
 
         private void StatsRow8_Click(object sender, RoutedEventArgs e)
         {
+            MessageBox.Show("Showing " + ConfigRow8.Text.ToString());
+            Beck_Option wnd = new Beck_Option();
+            wnd.Show();
 
+            this.Close();
         }
 
         private void StatsRow9_Click(object sender, RoutedEventArgs e)
         {
+            MessageBox.Show("Showing " + ConfigRow9.Text.ToString());
+            Beck_Option wnd = new Beck_Option();
+            wnd.Show();
 
+            this.Close();
         }
         private void StatsRow10_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Hi");
+            MessageBox.Show("Showing " + ConfigRow10.Text.ToString());
+            Beck_Option wnd = new Beck_Option();
+            wnd.Show();
+
+            this.Close();
         }
 
         private void StatsRow11_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Hi");
+            MessageBox.Show("Showing " + ConfigRow11.Text.ToString());
+            Beck_Option wnd = new Beck_Option();
+            wnd.Show();
+
+            this.Close();
         }
 
         private void StatsRow12_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Hi");
+            MessageBox.Show("Showing " + ConfigRow12.Text.ToString());
+            Beck_Option wnd = new Beck_Option();
+            wnd.Show();
+
+            this.Close();
+        }
+        private async void startupProg()
+        {
+            //init data
+            List<String> list = new List<String>();
+            //for (int i = 0 ;)
+            
         }
 
         private void Initials()
         {
-            //Check for ports that are open
-
-
             //Check for Web Proxy
             try
             {
@@ -118,7 +349,7 @@ namespace MainPage
                 MessageBox.Show(e.StackTrace);
             }
 
-            //
+            //Check for ports that are open
         }
         public static int GetAvailablePort(int startingPort)
         {
@@ -153,5 +384,96 @@ namespace MainPage
 
             return 0;
         }
+        // Update on click settings
+        //private void InstallUpdateSyncWithInfo()
+        //{
+        //    UpdateCheckInfo info = null;
+
+        //    if (ApplicationDeployment.IsNetworkDeployed)
+        //    {
+        //        ApplicationDeployment ad = ApplicationDeployment.CurrentDeployment;
+
+        //        try
+        //        {
+        //            info = ad.CheckForDetailedUpdate();
+
+        //        }
+        //        catch (DeploymentDownloadException dde)
+        //        {
+        //            MessageBox.Show("The new version of the application cannot be downloaded at this time. \n\nPlease check your network connection, or try again later. Error: " + dde.Message);
+        //            return;
+        //        }
+        //        catch (InvalidDeploymentException ide)
+        //        {
+        //            MessageBox.Show("Cannot check for a new version of the application. The ClickOnce deployment is corrupt. Please redeploy the application and try again. Error: " + ide.Message);
+        //            return;
+        //        }
+        //        catch (InvalidOperationException ioe)
+        //        {
+        //            MessageBox.Show("This application cannot be updated. It is likely not a ClickOnce application. Error: " + ioe.Message);
+        //            return;
+        //        }
+
+        //        if (info.UpdateAvailable)
+        //        {
+        //            Boolean doUpdate = true;
+
+        //            if (!info.IsUpdateRequired)
+        //            {
+        //                DialogResult dr = MessageBox.Show("An update is available. Would you like to update the application now?", "Update Available", MessageBoxButtons.OKCancel);
+        //                if (!(DialogResult.OK == dr))
+        //                {
+        //                    doUpdate = false;
+        //                }
+        //            }
+        //            else
+        //            {
+        //                // Display a message that the app MUST reboot. Display the minimum required version.
+        //                MessageBox.Show("This application has detected a mandatory update from your current " +
+        //                    "version to version " + info.MinimumRequiredVersion.ToString() +
+        //                    ". The application will now install the update and restart.",
+        //                    "Update Available", MessageBoxButtons.OK,
+        //                    MessageBoxIcon.Information);
+        //            }
+
+        //            if (doUpdate)
+        //            {
+        //                try
+        //                {
+        //                    ad.Update();
+        //                    MessageBox.Show("The application has been upgraded, and will now restart.");
+        //                    Application.Restart();
+        //                }
+        //                catch (DeploymentDownloadException dde)
+        //                {
+        //                    MessageBox.Show("Cannot install the latest version of the application. \n\nPlease check your network connection, or try again later. Error: " + dde);
+        //                    return;
+        //                }
+        //            }
+        //        }
+        //    }
+        //} 
+        //Type Ctrl K C
+
+        //protected override void (EventArgs e)
+        //{
+        //    base.OnLoad(e);
+        //    UpdateSession uSession = new UpdateSession();
+        //    IUpdateSearcher uSearcher = uSession.CreateUpdateSearcher();
+        //    uSearcher.Online = false;
+        //    try
+        //    {
+        //        ISearchResult sResult = uSearcher.Search("IsInstalled=1 And IsHidden=0");
+        //        textBox1.Text = "Found " + sResult.Updates.Count + " updates" + Environment.NewLine;
+        //        foreach (IUpdate update in sResult.Updates)
+        //        {
+        //            textBox1.AppendText(update.Title + Environment.NewLine);
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Console.WriteLine("Something went wrong: " + ex.Message);
+        //    }
+        //}
     }
 }
--- SECURE-iT-Yourself/MainPage/Beck_Option.xaml.cs
+++ SECURE-iT-Yourself/MainPage/Beck_Option.xaml.cs
@@ -25,23 +25,64 @@ namespace MainPage
         public Beck_Option()
         {
             InitializeComponent();
-            
+               
+            //To pull the data from previous window 
+
             //Add in the Title
             String OptionTitle = "Firewall Settings";
             lblMisconfigType.Content = OptionTitle;
             //Add in the Image
-           // Image setImage = new Image();
-            BitmapImage src = new BitmapImage();
-            src.BeginInit();
-            src.UriSource = new Uri("//Resource//Beck_Images//Background.jpg", UriKind.Absolute);
-            //setImage.Source = src;
-            StatusImage.Source = src;
+
+            int stat = 1;
+            ImageSet(stat, StatusImage);
 
         }
-
+        public void ImageSet(int Status, Image img)
+        {
+            if (Status.Equals(1))
+            {
+                try
+                {
+                    img.Source = new BitmapImage(new Uri("pack://application:,,,/Resources/Beck_Images/green-smiley-face-md.png"));
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("Problem " + e);
+                }
+            }
+            else if (Status.Equals(0))
+            {
+                try
+                {
+                    //Uri uri = new Uri("pack://application:,,,/Resources/Beck_Images/Background.jpg");
+                    //BitmapImage bi = new BitmapImage(uri);
+                    img.Source = new BitmapImage(new Uri("pack://application:,,,/Resources/Beck_Images/yellow-neutral-face-md.png"));
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("Problem " + e);
+                }
+            }
+            else if (Status.Equals(-1))
+            {
+                try
+                {
+                    //Uri uri = new Uri("pack://application:,,,/Resources/Beck_Images/Background.jpg");
+                    //BitmapImage bi = new BitmapImage(uri);
+                    img.Source = new BitmapImage(new Uri("pack://application:,,,/Resources/Beck_Images/red-smiley-face-md.png"));
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("Problem " + e);
+                }
+            }
+        }
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-           
+            Beck_NC wnd = new Beck_NC();
+            wnd.Show();
+
+            this.Close();
         }
     }
  }
